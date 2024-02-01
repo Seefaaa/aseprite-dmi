52577c4a8c6ad0dcec0dda5ea47848b1ad15b8fa
Seefaaa <sefa@outlook.ie>
2024-02-01 05:39:48 +0300

    CI workflow

--- build.cmd
+++ build.cmd
@@ -8,15 +8,18 @@ IF "%~1"=="--release" (
 	SET TARGET=release
 )
 
-IF "%~1"=="--skip" (
+IF "%~1"=="--ci" (
+	SET TARGET=%~2\release
 	SET SKIP=1
 )
+
+cd %~dp0
 
 IF NOT %SKIP% NEQ 0 (
 	rustc --version
 	IF %ERRORLEVEL% NEQ 0 (
 		echo "Rust is not installed."
-		exit /b
+		exit /b 1
 	)
 	cd lib
 	IF %TARGET%==debug (
@@ -27,41 +30,33 @@ IF NOT %SKIP% NEQ 0 (
 	cd ..
 	IF %ERRORLEVEL% NEQ 0 (
 		echo "lib build failed. Please check for errors."
-		rmdir /S /Q .dist
-		exit /b
+		exit /b 1
 	)
 )
 
 IF NOT EXIST lib\target\%TARGET%\lib.exe (
 	echo "lib.exe was not built. Please check for errors."
-	rmdir /S /Q .dist
-	exit /b
+	exit /b 1
 )
-
-IF EXIST .dist (
-	rmdir /S /Q .dist
-)
-mkdir .dist
-
-copy package.json .dist
-copy LICENSE .dist
-
-copy lib\target\%TARGET%\lib.exe .dist
-
-xcopy /E scripts\ .dist\scripts\
 
 IF EXIST dist (
 	rmdir /S /Q dist
 )
+
 mkdir dist
+mkdir dist\unzipped
 
-powershell Compress-Archive -Path ".dist\*" -DestinationPath "dist\%EXTENSION_NAME%.zip" -Force
+copy package.json dist\unzipped
+copy LICENSE dist\unzipped
 
-rmdir /S /Q .dist
+copy lib\target\%TARGET%\lib.exe dist\unzipped
 
-cd dist
-IF EXIST %EXTENSION_NAME%.aseprite-extension (
-	del %EXTENSION_NAME%.aseprite-extension
+xcopy /E scripts\ dist\unzipped\scripts\
+
+powershell Compress-Archive -Path "dist\unzipped\*" -DestinationPath "dist\%EXTENSION_NAME%.zip" -Force
+
+IF EXIST dist\%EXTENSION_NAME%.aseprite-extension (
+	del dist\%EXTENSION_NAME%.aseprite-extension
 )
-copy %EXTENSION_NAME%.zip %EXTENSION_NAME%.aseprite-extension
-cd ..
+
+copy dist\%EXTENSION_NAME%.zip dist\%EXTENSION_NAME%.aseprite-extension
